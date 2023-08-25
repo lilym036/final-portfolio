bbae56a4e051bba0db4115baeadd02d908dd2ead
Lilibeth Mejia <mejialily036@gmail.com>
2023-08-25 17:28:33 -0500

    added more links

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -21,22 +21,22 @@ export default function Projects() {
                         <h3 className="projectName">Refresh</h3>
                     </div>
                 </a>
-                <a href="#" id="picture-3" alt="picture of plant-3">
+                <a href="https://drive.google.com/file/d/18fa0vCSbyXQggGjM8ZczqHRbAWlkniBs/view" id="picture-3" alt="picture of plant-3">
                     <div>
                         <h3 className="projectName">README Generator</h3>
                     </div>
                 </a>
-                <a href="#" id="picture-4" alt="picture of plant-4">
+                <a href="https://drive.google.com/file/d/10tM9NZmGm-_hg0cqQ9kr7ZCoaDK_mZFS/view" id="picture-4" alt="picture of plant-4">
                     <div>
                         <h3 className="projectName">Employee Tracker</h3>
                     </div>
                 </a>
-                <a href="#" id="picture-5" alt="picture of plant-5">
+                <a href="https://drive.google.com/file/d/1Dcp6m1oxKhnPmOeHlpBBQXoM0tZayLeW/view" id="picture-5" alt="picture of plant-5">
                     <div>
                         <h3 className="projectName">Logo Maker</h3>
                     </div>
                 </a>
-                <a href="#" id="picture-6" alt="picture of plant-5">
+                <a href="https://drive.google.com/file/d/13bc6XDl0Pg2oKZCkXr_7WORAzqbE9Oyc/view" id="picture-6" alt="picture of plant-5">
                     <div>
                         <h3 className="projectName">Social Network API</h3>
                     </div>
