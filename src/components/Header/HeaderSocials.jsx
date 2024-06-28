a03a8e8973adc8e6f61380502bad5a6b5112eddc
Lilibeth Neal <mejialily036@gmail.com>
2024-06-28 11:42:38 -0500

    made changes

--- src/components/Header/HeaderSocials.jsx
+++ src/components/Header/HeaderSocials.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
-import {FaStackOverflow} from 'react-icons/fa6'
+// import {FaStackOverflow} from 'react-icons/fa6'
 
 const HeaderSocials = () => {
   return (
